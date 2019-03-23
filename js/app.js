5f248d30e1a173162aefd58114efb367fbcad02d
deartist22 <deartist22@gmail.com>
2019-03-23 17:11:04 -0500

    Update for basic structure of Color Game
    Add title to index.html
    Change class square to squares
    Create  logic for color pick on app.js with event listener.

--- js/app.js
+++ js/app.js
@@ -1,1 +1,29 @@
-alert("hello");
+var colors = [
+	"rgb(255,0,0)",
+	"rgb(255, 255, 0)",
+	"rgb(0, 255, 0)",
+	"rgb(0, 255, 255)",
+	"rgb(0, 0, 255)",
+	"rgb(255, 0, 255)"
+]
+
+var squares = document.querySelectorAll(".squares");
+var pickerColor = colors[3];
+var colorDisplay = document.getElementById("colorDisplay");
+
+colorDisplay.textContent = pickerColor;
+
+for(var i = 0; i < squares.length; i++) {
+	squares[i].style.backgroundColor = colors[i];
+
+	squares[i].addEventListener("click", function() {
+		//grab color of clicked square
+		clickedColor = this.style.background;
+		//compare color to pickedColor
+		if(clickedColor === pickedColor){
+			alert("Correct!")
+		} else {
+			alert("Wrong!")
+		}
+	})
+}
