7dcbaf56e00ee5ab4fbbb8aa598000314cc0c366
deartist22 <deartist22@gmail.com>
2019-03-25 06:42:57 -0500

    Reset button added
    Reset button will reset color and style.background on all squares

--- js/app.js
+++ js/app.js
@@ -5,6 +5,21 @@ var pickedColor = pickColor();
 var colorDisplay = document.getElementById("colorDisplay");
 var messageDisplay = document.querySelector("#message");
 var h1 = document.querySelector("h1");
+var reset = document.querySelector("#reset");
+
+reset.addEventListener("click", function() {
+	//generate all new colors
+	colors = generateRandomColors(6);
+	//pick a new random color from array
+	pickedColor = pickColor();
+	//change colors of squares
+	colorDisplay.textContent = pickedColor;
+	for(var i = 0; i < squares.length; i++) {
+		squares[i].style.backgroundColor = colors[i];
+	}
+	h1.style.backgroundColor = "#232323";
+	reset.textContent = "New Colors";
+})
 
 colorDisplay.textContent = pickedColor;
 
@@ -17,6 +32,7 @@ for(var i = 0; i < squares.length; i++) {
 		//compare color to pickedColor
 		if(clickedColor === pickedColor){
 			messageDisplay.textContent = "Correct";
+			reset.textContent = "Play Again!";
 			changeColors(clickedColor);
 			h1.style.backgroundColor = clickedColor;
 		} else {
